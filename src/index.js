db40bba8b7add8cd788f033e5e05d4ed30b35825
sweta530 <sweta.gohel53@gmail.com>
2024-04-28 13:46:17 +0530

    HashRouter added

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import reportWebVitals from './reportWebVitals';
 // Redux Store
 import { store } from '../src/store/store'
 import { Provider } from 'react-redux'
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
